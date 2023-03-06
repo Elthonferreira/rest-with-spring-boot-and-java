2822e03a2e51e629d3436c6a6fde3224e282a7c0
Elthon <elthonferreira18@gmail.com>
2023-03-06 19:17:44 -0300

    Refatoração do controller

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-sec6/src/main/java/br/com/elthon/controllers/PersonController.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-sec6/src/main/java/br/com/elthon/controllers/PersonController.java
@@ -4,7 +4,12 @@ package br.com.elthon.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,38 +26,37 @@ public class PersonController {
 	private PersonServices personServices;
 	//private PersonServices personServices = new PersonServices();
 	
-	@RequestMapping(method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE) // RequestMapping vai mapear uma requisição para um método
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE) // RequestMapping vai mapear uma requisição para um método
 	public List<Person> findAll() throws Exception {
 	
 		return personServices.findAll();
 	}
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable (value = "id") Long id) throws Exception {
 	
 		return personServices.findById(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE) 
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			     produces = MediaType.APPLICATION_JSON_VALUE) 
 	public Person create(@RequestBody Person person) throws Exception {
 	
 		return personServices.create(person);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE) 
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			    produces = MediaType.APPLICATION_JSON_VALUE) 
 	public Person update(@RequestBody Person person) throws Exception {
 	
 		return personServices.update(person);
 	}
 	
-	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE) 
-	public void delete(@PathVariable (value = "id") Long id) throws Exception {
+	@DeleteMapping(value = "/{id}") 
+	public ResponseEntity<?> delete(@PathVariable (value = "id") Long id) throws Exception {
 	
 		personServices.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	
