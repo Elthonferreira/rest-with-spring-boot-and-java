d88a12b46c5f18e891560146fd17aa7ab3e4d48a
Elthon <elthonferreira18@gmail.com>
2023-03-09 00:43:06 -0300

    Adição de migrations com flyway (ainda está com erro, estou tentando solucionar)

---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__insert_table_person.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `person` VALUES
+(1,'SLM','Elthon','Male','Ferreira'),
+(3,'SLM','Grace','Female','Kaller'),
+(6,'Limoeiro','Eder','Male','Lucena'),
+(8,'Recife','Anderson','Male','Mister'),
+(9,'SLM','Angelo','Male','Hugo'),
+(10,'SLM','Ricardo','Male','Alves'),
+(11,'Camaragibe','Thiago','Male','Senna'),
+(12,'SLM','Fábio','Male','Alves');
