cb34b7685677421de23a75df20ad5cdb11da1169
Elthon <elthonferreira18@gmail.com>
2023-03-18 19:32:21 -0300

    Exemplos de customização de json, mas comentei porque não vou usar

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/java/br/com/elthon/data/vo/v1/PersonVO.java
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/java/br/com/elthon/data/vo/v1/PersonVO.java
@@ -3,14 +3,26 @@ package br.com.elthon.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+//@JsonPropertyOrder({"id", "first_name", "last_name", "gender", "address"}) // altera a ordem de atributos na requisição
 public class PersonVO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+
+	//@JsonProperty("first_name") // altera o nome do atributo na requisição
 	private String firstName;
+
+	//@JsonProperty("last_name") // altera o nome do atributo na requisição
 	private String lastName;
+
+	//@JsonIgnore
 	private String address;
+
 	private String gender;
 	
 	public PersonVO() {
