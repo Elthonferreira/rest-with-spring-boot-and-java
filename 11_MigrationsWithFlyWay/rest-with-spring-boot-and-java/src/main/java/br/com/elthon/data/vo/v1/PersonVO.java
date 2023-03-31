2943850e0f013300479b826d9606659acc14637d
Elthon <elthonferreira18@gmail.com>
2023-03-30 23:40:33 -0300

    Implementação de HATEOAS para retornar informações adicionais nas requisições.

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/java/br/com/elthon/data/vo/v1/PersonVO.java
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/java/br/com/elthon/data/vo/v1/PersonVO.java
@@ -6,84 +6,83 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+import org.springframework.hateoas.RepresentationModel;
 
-//@JsonPropertyOrder({"id", "first_name", "last_name", "gender", "address"}) // altera a ordem de atributos na requisição
-public class PersonVO implements Serializable{
+@JsonPropertyOrder({"id", "first_name", "last_name", "gender", "address"}) // altera a ordem de atributos na requisição
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Long id;
+    @JsonProperty("id")
+    @Mapping("id")
+    private Long key;
 
-	//@JsonProperty("first_name") // altera o nome do atributo na requisição
-	private String firstName;
+    //@JsonProperty("first_name") // altera o nome do atributo na requisição
+    private String firstName;
 
-	//@JsonProperty("last_name") // altera o nome do atributo na requisição
-	private String lastName;
+    //@JsonProperty("last_name") // altera o nome do atributo na requisição
+    private String lastName;
 
-	//@JsonIgnore
-	private String address;
+    //@JsonIgnore
+    private String address;
 
-	private String gender;
-	
-	public PersonVO() {
-	}
+    private String gender;
 
-	public Long getId() {
-		return id;
-	}
+    public PersonVO() {
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getKey() {
+        return key;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public void setKey(Long key) {
+        this.key = key;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public String getGender() {
-		return gender;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
+    public String getGender() {
+        return gender;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(address, firstName, gender, id, lastName);
-	}
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PersonVO other = (PersonVO) obj;
-		return Objects.equals(address, other.address) && Objects.equals(firstName, other.firstName)
-				&& Objects.equals(gender, other.gender) && Objects.equals(id, other.id)
-				&& Objects.equals(lastName, other.lastName);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PersonVO personVO = (PersonVO) o;
+        return Objects.equals(key, personVO.key) && Objects.equals(firstName, personVO.firstName) && Objects.equals(lastName, personVO.lastName) && Objects.equals(address, personVO.address) && Objects.equals(gender, personVO.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), key, firstName, lastName, address, gender);
+    }
 }
--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/java/br/com/elthon/services/PersonServices.java
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java/src/main/java/br/com/elthon/services/PersonServices.java
@@ -3,6 +3,7 @@ package br.com.elthon.services;
 import java.util.List;
 import java.util.logging.Logger;
 
+import br.com.elthon.controllers.PersonController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ import br.com.elthon.mapper.DozerMapper;
 import br.com.elthon.model.Person;
 import br.com.elthon.data.vo.v1.PersonVO;
 import br.com.elthon.repositories.PersonRepository;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class PersonServices {
@@ -23,31 +27,44 @@ public class PersonServices {
 	public List<PersonVO> findAll() {
 		logger.info("Finding all people!");
 		
-		return DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+		var persons = DozerMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+
+		persons.stream().forEach(p -> {
+			try {
+				p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
+
+		return persons;
 	}
 
-	public PersonVO findById(Long id) {
+	public PersonVO findById(Long id) throws Exception {
 		logger.info("Finding one person!");
 		
 		var entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		
-		return DozerMapper.parseObject(entity, PersonVO.class);
+		PersonVO vo = DozerMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
 	}
 	
-	public PersonVO create(PersonVO person) {
+	public PersonVO create(PersonVO person) throws Exception {
 		logger.info("Creating one person!");
 		
 		var entity = DozerMapper.parseObject(person, Person.class);
 		
 		var vo = DozerMapper.parseObject(personRepository.save(entity), PersonVO.class);
-		
+
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 	
-	public PersonVO update(PersonVO person) {
+	public PersonVO update(PersonVO person) throws Exception {
 		logger.info("Updating one person!");
 		
-		var personToUpdate = personRepository.findById(person.getId())
+		var personToUpdate = personRepository.findById(person.getKey())
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		
 		personToUpdate.setFirstName(person.getFirstName());
@@ -56,7 +73,8 @@ public class PersonServices {
 		personToUpdate.setGender(person.getGender());
 		
 		var vo = DozerMapper.parseObject(personRepository.save(personToUpdate), PersonVO.class);
-		
+
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 	
